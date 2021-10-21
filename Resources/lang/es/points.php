c9efddd0d5b72dd8a293fcd4597ec9d073eedf04
William Verde <williamverde10@gmail.com>
2021-10-21 12:58:54 -0400

    Updates: lang

---
+++ Resources/lang/es/points.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'list resource' => 'Listar puntos',
+    'create resource' => 'Crear puntos',
+    'edit resource' => 'Editar puntos',
+    'destroy resource' => 'Eliminar puntos',
+    'title' => [
+        'points' => 'Point',
+        'create point' => 'Crear punto',
+        'edit point' => 'Editar punto',
+    ],
+    'button' => [
+        'create point' => 'Crear punto',
+    ],
+    'table' => [
+    ],
+    'form' => [
+    ],
+    'messages' => [
+    ],
+    'validation' => [
+    ],
+];
